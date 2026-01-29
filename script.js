57c02091272469c2d81dce9010ac3c7c3f706b66
Kamal Rathore <mr.rathoreji52@gmail.com>
2026-01-29 19:16:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-//your code here!
+const ol = document.querySelector("#infi-list");
+let i = 1;
 
+// initial items
+for (let j = 0; j < 10; j++) {
+  const li = document.createElement("li");
+  li.textContent = `Item ${i++}`;
+  ol.append(li);
+}
+
+function addItems() {
+  for (let j = 0; j < 2; j++) {
+    const li = document.createElement("li");
+    li.textContent = `Item ${i++}`;
+    ol.append(li);
+  }
+}
+
+// 👇 SCROLL ON CONTAINER
+ol.addEventListener("scroll", () => {
+  if (ol.scrollTop + ol.clientHeight >= ol.scrollHeight - 5) {
+    addItems();
+  }
+});
